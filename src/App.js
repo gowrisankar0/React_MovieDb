9530b6d9219981002d92f10aeb2fadf1aabc29a1
gowrisankar0 <gsankar9797@gmail.com>
2023-12-13 11:47:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [item, setItem] = useState("");
+  const [todo, setTodo] = useState(()=>{
+    const localVal =localStorage.getItem("ITEM");
+    if(localVal===null) return []
+    return JSON.parse(localVal)
+
+  });
+
+
+
+  useEffect(()=>{
+    localStorage.setItem("ITEM",JSON.stringify(todo))
+  },[todo])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setTodo((curr) => {
+      return [
+        ...curr,
+        { id: crypto.randomUUID(), title: item, completed: false },
+      ];
+    });
+
+    setItem("");
+  };
+  console.log(todo);
+
+  
+  const toggleTodo = (id,completed)=>{
+    setTodo((currTodo)=>{
+      return currTodo.map((todo)=>{
+        if(todo.id===id){
+          return {...todo,completed}
+        }
+        return todo;
+      })
+    })
+
+  }
+
+
+function deleteTodos(id) {
+
+  setTodo(currentTodo=>{
+    return currentTodo.filter(todos=>todos.id!==id)
+  })
+  
 }
+
+  return (
+    <>
+      <form className="new-item-form" onSubmit={handleSubmit}>
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input
+            type="text"
+            id="item"
+            value={item}
+            onChange={(e) => setItem(e.target.value)}
+          />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1>To-Do List</h1>
+      <ul className="list">
+        {todo.length===0 && <h2>No ToDo's</h2>}
+        {todo.map((curr) => {
+          return (
+            <li key={curr.id}>
+              <label>
+                <input type="checkbox" checked={curr.completed} 
+                onChange={(e)=>toggleTodo(curr.id,e.target.checked)}
+                />
+                {curr.title}
+              </label>
+              <button
+                className="btn btn-danger"
+                onClick={() => deleteTodos(curr.id)}
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
 
 export default App;
